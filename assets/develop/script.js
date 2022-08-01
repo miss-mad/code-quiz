2852c72feaaafb0cb33a528828a7d088daa76b84
miss-mad <mmlh.harris@gmail.com>
2022-08-01 18:05:18 -0400

    create function for asking questions

--- assets/develop/script.js
+++ assets/develop/script.js
@@ -26,37 +26,60 @@ var timer = document.querySelector(".timer");
 var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
 
-quiz.setAttribute;
+// quiz.append(questions[0]);
 
 var totalTime = 5;
 var isIncorrect = false;
 
-
-function countdownTimer() {
-    var countdownFunction = setInterval(function () {
-        if (isIncorrect) {
-            totalTime - 4;
-            isIncorrect = false;
-        }
-        totalTime--;
-        timer.textContent = "Time: " + totalTime;
-        if (totalTime >= 0) {
-            console.log(totalTime);
-        }
-        
-        if (totalTime === 0) {
-            clearInterval(countdownFunction);
-            timer.textContent = "Time is up!";
-            //   showImage();
-        }
-    }, 1000);
+function askQuestions() {
+  if (start.addEventListener("click", countdownTimer) === true) {
+    questions.textContent = questions[0];
+    if (answerChoices === "Brandon Erlch") {
+      quiz.textContent = "Correct!";
+    } else {
+      isIncorrect;
+      quiz.textContent = "Incorrect! Minus 5 seconds";
+    }
+    questions.textContent = questions[1];
+    if (answerChoices === ".shift") {
+      quiz.textContent = "Correct!";
+    } else {
+      isIncorrect;
+      quiz.textContent = "Incorrect! Minus 5 seconds";
+    }
+    questions.textContent = questions[2];
+    if (answerChoices === "function name + ();") {
+      quiz.textContent = "Correct!";
+    } else {
+      isIncorrect;
+      quiz.textContent = "Incorrect! Minus 5 seconds";
+    }
+  }
 }
 
-countdownTimer();
+function countdownTimer() {
+  var countdownFunction = setInterval(function () {
+    if (isIncorrect) {
+      totalTime - 4;
+      isIncorrect = false;
+    }
+    totalTime--;
+    timer.textContent = "Time: " + totalTime;
+    if (totalTime >= 0) {
+      console.log(totalTime);
+    }
+
+    if (totalTime === 0) {
+      clearInterval(countdownFunction);
+      timer.textContent = "Time is up!";
+    }
+  }, 1000);
+}
+
 start.addEventListener("click", countdownTimer);
 
 // Function to create and append colorsplosion image
-// function showImage() {
+// function viewHighscores() {
 //   timer.textContent = " ";
 //   var imgEl = document.createElement("img");
 //   imgEl.setAttribute("src", "images/image_1.jpg");
