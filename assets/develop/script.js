0c7f92b815ab41223429691d3ddc4a85590f48d1
miss-mad <mmlh.harris@gmail.com>
2022-08-01 18:04:16 -0400

    create attribute selectors

--- assets/develop/script.js
+++ assets/develop/script.js
@@ -12,39 +12,48 @@ var questions = [
   {
     questionThree: "How do you call a function?",
     answerChoices: [
+      "var call = function(event) {}",
+      "console.log(function);",
       "function name + ();",
       "Pick up the phone",
-      "var = function(event) {}",
-      "console.log(function);",
     ],
     answerCorrect: "function name + ();",
   },
 ];
 
+var highscores = document.querySelector(".highscores");
 var timer = document.querySelector(".timer");
-var totalTime = 1;
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+
+quiz.setAttribute;
+
+var totalTime = 5;
 var isIncorrect = false;
 
-function countdownTimer() {
-  var countdownFunction = setInterval(function () {
-    if (isIncorrect) {
-      totalTime - 4;
-    }
-    totalTime--;
-    timer.textContent = "Time: " + totalTime;
-    if (totalTime >= 0) {
-      console.log(totalTime);
-    }
 
-    if (totalTime === 0) {
-      clearInterval(countdownFunction);
-      timer.textContent = "Time is up!";
-      //   showImage();
-    }
-  }, 1000);
+function countdownTimer() {
+    var countdownFunction = setInterval(function () {
+        if (isIncorrect) {
+            totalTime - 4;
+            isIncorrect = false;
+        }
+        totalTime--;
+        timer.textContent = "Time: " + totalTime;
+        if (totalTime >= 0) {
+            console.log(totalTime);
+        }
+        
+        if (totalTime === 0) {
+            clearInterval(countdownFunction);
+            timer.textContent = "Time is up!";
+            //   showImage();
+        }
+    }, 1000);
 }
 
 countdownTimer();
+start.addEventListener("click", countdownTimer);
 
 // Function to create and append colorsplosion image
 // function showImage() {
