99b87afe2686944f71a41ba3c0782e769634b26b
miss-mad <mmlh.harris@gmail.com>
2022-08-01 18:02:41 -0400

    finish timer function and create questions object

--- assets/develop/script.js
+++ assets/develop/script.js
@@ -1,26 +1,56 @@
+var questions = [
+  {
+    questionOne: "Who invented JavaScript?",
+    answerChoices: ["Mickey Mouse", "Madonna", "Brandon Erlch", "Rhiannon"],
+    answerCorrect: "Brandon Erlch",
+  },
+  {
+    questionTwo: "Which array method removes the first element from the array?",
+    answerChoices: [".split", ".push", ".pop", ".shift"],
+    answerCorrect: ".shift",
+  },
+  {
+    questionThree: "How do you call a function?",
+    answerChoices: [
+      "function name + ();",
+      "Pick up the phone",
+      "var = function(event) {}",
+      "console.log(function);",
+    ],
+    answerCorrect: "function name + ();",
+  },
+];
+
 var timer = document.querySelector(".timer");
-var totalTime = 60;
+var totalTime = 1;
+var isIncorrect = false;
 
-function timeRemaining() {
-  var countdown = setInterval(function() {
-    timer.textContent = "Time: " + totalTime;
+function countdownTimer() {
+  var countdownFunction = setInterval(function () {
+    if (isIncorrect) {
+      totalTime - 4;
+    }
     totalTime--;
-
-    if(totalTime === 0) {
-      clearInterval(countdown);
-    //   showImage();
+    timer.textContent = "Time: " + totalTime;
+    if (totalTime >= 0) {
+      console.log(totalTime);
     }
 
+    if (totalTime === 0) {
+      clearInterval(countdownFunction);
+      timer.textContent = "Time is up!";
+      //   showImage();
+    }
   }, 1000);
 }
 
+countdownTimer();
+
 // Function to create and append colorsplosion image
-function showImage() {
-  timer.textContent = " ";
-  var imgEl = document.createElement("img");
-  imgEl.setAttribute("src", "images/image_1.jpg");
-  mainEl.appendChild(imgEl);
+// function showImage() {
+//   timer.textContent = " ";
+//   var imgEl = document.createElement("img");
+//   imgEl.setAttribute("src", "images/image_1.jpg");
+//   mainEl.appendChild(imgEl);
 
-}
-
-timeRemaining();
+// }
